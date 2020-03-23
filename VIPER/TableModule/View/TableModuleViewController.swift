749b104baf5b310126eee4e13c65788bee7344bb
yudintsev.dv <dyudintsev@gmail.com>
2020-03-24 01:02:18 +0700

    add

--- VIPER/TableModule/View/TableModuleViewController.swift
+++ VIPER/TableModule/View/TableModuleViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableModuleViewController: UIViewController {
+    
+    @IBOutlet weak var table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func setupTable() {
+    }
 }
+
 
